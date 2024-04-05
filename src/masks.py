0500bd6d89848fc3e981fcf9ac001ab59e6b5655
Student Free <evgenijrybakov01@gmail.com>
2024-04-05 16:51:42 +0300

    update functions

---
+++ src/masks.py
@@ -0,0 +1,23 @@
+import tests
+
+
+def mask_cards(masked_card: str) -> str:
+    '''Принимает номер карты и возвращает изменённый зашифрованный номер
+    и считывает номер счета в банке и возвращает зашифрованный
+    '''
+    mask_account_card = (
+            masked_card[:4]
+            + " "
+            + masked_card[4:6]
+            + "**"
+            + " "
+            + "****"
+            + " "
+            + masked_card[12:]
+    )
+    return mask_account_card
+
+
+def mask_bills(masked_bill: str) -> str:
+    return "**" + masked_bill[-4:]
+
