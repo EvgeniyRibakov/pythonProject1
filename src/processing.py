eb1e84f601cfaeb26df59756cbe856d4023716da
EvgeniyRibakov <139446358+EvgeniyRibakov@users.noreply.github.com>
2024-04-18 15:16:46 +0300

    Update processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,8 @@
-from typing import List
-from datetime import datetime
-
-
 def get_key_state(data: dict, state: str = "EXECUTED") -> list:
     return [item for item in data if item.get("state") == state]
 
 
-def sorted_dates(data: List[dict], reverse: bool = True) -> List[dict]:
-    return sorted(data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
+def sorted_dates(data: list[dict], reverse: bool = False) -> list[dict]:
+    sorted_data = sorted(data, key=lambda x: x["date"], reverse=reverse)
+    return sorted_data
+
