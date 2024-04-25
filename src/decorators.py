f97e70a045dba2266906080e2edacaf281276931
EvgeniyRibakov <evgenijrybakov01@gmail.com>
2024-04-25 10:39:51 +0300

    Add two files to .gitignore

---
+++ src/decorators.py
@@ -0,0 +1,35 @@
+# import functools
+# import datetime
+#
+# def log(filename=None):
+#     def decorator(func):
+#         @functools.wraps(func)
+#         def wrapper(*args, **kwargs):
+#             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#             func_args = ', '.join(map(repr, args))
+#             func_kwargs = ', '.join(f"{k}={v!r}" for k, v in kwargs.items())
+#             all_args = ', '.join(filter(None, [func_args, func_kwargs]))
+#
+#             try:
+#                 result = func(*args, **kwargs)
+#                 log_message = f"{timestamp} - {func.__name__}({all_args}) -> {result}\n"
+#             except Exception as e:
+#                 log_message = f"{timestamp} - ERROR in {func.__name__}({all_args}): {e}\n"
+#                 if filename:
+#                     with open(filename, 'a') as file:
+#                         file.write(log_message)
+#                 else:
+#                     print(log_message)
+#                 raise
+#
+#             if filename:
+#                 with open(filename, 'a') as file:
+#                     file.write(log_message)
+#             else:
+#                 print(log_message)
+#
+#             return result
+#
+#         return wrapper
+#
+#     return decorator
